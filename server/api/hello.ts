fd10b40161c3371eab57a59265b1f248dbbae1d0
Maratib Ali Khan <maratib@gmail.com>
2021-12-23 03:20:14 +0500

    Prisma and API added

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,6 +1,11 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { UserService } from "./services/user.service";
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
+  const userService = new UserService();
+  const users = await userService.findAll();
+  console.log(users);
+  res.setHeader("Content-Type", "application/json");
   res.statusCode = 200;
-  res.end("Works!");
+  res.end(JSON.stringify(users));
 };
